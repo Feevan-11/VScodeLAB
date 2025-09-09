c52f4beeab5256910b83ad94e54f87f92ec521a1
fyf <feevan11@163,com>
2025-09-09 10:09:09 +0800

    TL_DDR

--- python/asm/ROM.asm
+++ python/asm/ROM.asm
@@ -2,20 +2,20 @@ lui x1, 0xFF004
 lui x2, 0xFF004
 lui x3, 0x1
 lui x4, 0x1
-lui x5, 0x40000
-lui x6, 0xF4000
-lui x7, 0x80000
-lui x8, 0xF0001
+lui x5, 0xF0000
+lui x6, 0x40000
+lui x7, 0xF0000
+lui x8, 0x80000
 addi x1, x1, 0x400
 addi x2, x2, 0x440
 addi x3, x3, 0x0
 addi x4, x4, 0x0
-addi x5, x5, 0x0
+addi x5, x5, 0x400
 addi x6, x6, 0x0
-addi x7, x7, 0x0
+addi x7, x7, 0x600
 addi x8, x8, 0x0
 lui x9, 0x0
-lui x10, 0x1
+lui x10, 0x0
 sw x3,  0x0(x2)
 addi x0, x0, 0 
 addi x0, x0, 0        
@@ -23,7 +23,7 @@ addi x0, x0, 0
 addi x0, x0, 0        
 addi x0, x0, 0
 addi x9, x9, 0x200
-addi x10, x10, 0x800
+addi x10, x10, 0x200
 sw x4, 0x4(x2)
 addi x0, x0, 0 
 addi x0, x0, 0
@@ -92,160 +92,9 @@ addi x17, x17, 0x400
 addi x18, x18, 0x0
 addi x0, x0, 0        
 addi x0, x0, 0 
-sw x3, 0x4(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x4, 0x0(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x11, 0x18(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x12, 0x20(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x13, 0x28(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0 
 addi x0, x0, 0 
 addi x0, x0, 0 
 addi x0, x0, 0
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-lp.setup  x1,  100
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-lp.goto x1, -0x10
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x3, 0x4(x1)    
-addi x0, x0, 0  
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x4, 0x0(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-sw x14, 0x18(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x15, 0x20(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x16, 0x28(x1)    
-addi x0, x0, 0
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-lp.setup  x1,  100
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-lp.setup  x2,  1
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-lp.goto x2, -0x10
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-lp.goto x1, -0x40 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x3, 0x4(x1)    
-addi x0, x0, 0  
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x4, 0x0(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-sw x17, 0x18(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x18, 0x20(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0        
-addi x0, x0, 0 
-sw x16, 0x28(x1)    
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-lp.setup  x1,  100
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-lp.setup  x2,  1
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-lp.goto x2, -0x10
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-lp.goto x1, -0x40 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-lp.setup  x1, 50
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0
-addi x0, x0, 0 
-addi x0, x0, 0 
-addi x0, x0, 0 
-lp.goto x1, 0xC30
 addi x0, x0, 0 
 addi x0, x0, 0 
 addi x0, x0, 0 
@@ -256,5 +105,152 @@ addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0 
 addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0 
+addi x0, x0, 0
 addi x0, x0, 0 
 addi x0, x0, 0
